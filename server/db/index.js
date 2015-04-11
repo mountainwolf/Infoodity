31c4937852425ab725dcc6636c0ca3b9db4a2a85
sperrow <mattjsperry@gmail.com>
2015-04-10 18:59:45 -0700

    [feat] Add restaurantInfo endpoint, add instructions on how to initialize local db

--- server/db/index.js
+++ server/db/index.js
@@ -16,7 +16,20 @@ module.exports = {
       });
     });
   },
-  restaurantReviews: function(query, cb) {
+  restaurantInfo: function(id, cb) {
+    var queryStr = "SELECT * FROM restaurants WHERE id = " + id + ";";
+    pg.connect(dbUrl, function(err, client, done) {
+      client.query(queryStr, function(err, result) {
+        done();
+        if( err ) {
+          console.error(err);
+        } else {
+          cb(result.rows);
+        }
+      });
+    });
+  },
+  restaurantReviews: function(id, cb) {
     var queryStr = "SELECT reviews.rating, reviews.dish_name, reviews.text, reviews.image_url, users.user_name, restaurants.name FROM reviews INNER JOIN users ON reviews.user_id = users.id INNER JOIN restaurants ON reviews.restaurant_id = restaurants.id WHERE restaurant_id = " + query + ";";
     pg.connect(dbUrl, function(err, client, done) {
       client.query(queryStr, function(err, result) {
