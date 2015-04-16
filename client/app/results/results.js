090c59f9f2233f7fc2882bfdbe82ab4c828418b1
sperrow <mattjsperry@gmail.com>
2015-04-16 15:56:57 -0700

    [feat] add client folder to master repo, fix html script tags

---
+++ client/app/results/results.js
@@ -0,0 +1,23 @@
+    infoodity.controller('ResultsController',['$scope', 'Results', '$location', '$log', function ($scope, Results, $location, $log){
+      $log.log('Hello!');
+//      var nameSearched = $location.path().split('=').pop();
+//
+//      $scope.data = {} // getData
+//
+//      $scope.getResults = function (name) { // function used to populate data field (is parameter data necessary??)
+//        console.log(name);
+//        Results.getResults(name) // call $http request
+//          .then(function (data) {
+//            console.log(data);
+//            $scope.results = data;
+//            console.log(data); // on return of $http call, populate data
+//          })
+//          .catch(function (error) {
+//            console.error(error); // if error on return of $http call, return error
+//          });
+//      };
+//
+//      $scope.getResults(nameSearched);
+
+  // reroute to restaurant page
+    }]);
---
+++ client/app/submitReview/submitReview.js
@@ -0,0 +1,6 @@
+angular.module('infoodity.submitReview', [])
+
+  .controller('SubmitReviewController', ['$scope', function ($scope){
+
+}]);
+
