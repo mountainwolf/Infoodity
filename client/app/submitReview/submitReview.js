ee58900e16f1ceff35b65e6a42d937120f57166c
Sean Conroy <conroysean@gmail.com>
2015-04-20 10:49:48 -0700

    [feat] finish post functionality, adjust front end

--- client/app/submitReview/submitReview.js
+++ client/app/submitReview/submitReview.js
@@ -1,14 +1,17 @@
-infoodity.controller('SubmitReviewController', ['$scope', 'Reviews', 'Restaurant', function($scope, Reviews, Restaurant) {
-  var data = {
-    dish_name: $scope.dish,
-    image_url: $scope.img, //I don't think this will work...this might even be a seperate post request.
-    name: $scope.name,
-    rating: $scope.stars, //this may be retrieved from ng-click event.
-    restaurantID: Restaurant.getRestaurant().id,
-    stars: $scope.stars, //how does this differ from rating property? (copied from testdata object)
-    text: $scope.review,
-    user_name: $scope.user
+infoodity.controller('SubmitReviewController', ['$scope', 'Reviews', 'Restaurant', '$state', function($scope, Reviews, Restaurant, $state) {
+
+  $scope.sendReview = function(){
+    var fd = new FormData();
+    fd.append('file', $('.image')[0].files[0], 'image');
+    fd.append('dish_name', $scope.dishname);
+    fd.append('text', $scope.review);
+    fd.append('rating', $scope.stars);
+    fd.append('restaurant_id', Restaurant.getRestaurant().id);
+    fd.append('user_id', 1);
+    Reviews.postReview(fd)
+      .then(function(){
+        $state.go('restaurantPage');
+      });
   };
-  Reviews.postReview(data);
 }]);
 
